da561fdb9a770f9e594019d7d17d97b834e71836
anonymedelaregion <anonyme201504@gmail.com>
2023-08-05 23:06:50 -0300

    azerty

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+private _musicfileUrl="myucifileurl"
+private _musiccount="myucifileurl"
+  constructor(private httpClient: HttpClient) {}
+  async uploadPhoto(fileChangeEvent:any) {
+    // Get a reference to the file that has just been added to the input
+    const photo = fileChangeEvent.target.files[0];
 
-  constructor() {}
+    // Create a form data object using the FormData API
+    let formData = new FormData();
 
+    // Add the file that was just added to the form data
+    formData.append("music", photo, photo.name);
+    formData.append("photo", photo, photo.name);
+        this.httpClient.get(this._musicfileUrl)
+            .subscribe(googleVolumeListResponse => {
+                this._musiccount = googleVolumeListResponse.totalItems;
+                })
+}
 }
